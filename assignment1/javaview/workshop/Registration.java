389e9befdd3be2702dcbfac93f12053057ae365b
Tim van Rossum <trvanrossum@gmail.com>
2016-05-21 22:50:22 +0200

    Implemented point-to-plane correctly now, I think.

--- assignment1/javaview/workshop/Registration.java
+++ assignment1/javaview/workshop/Registration.java
@@ -29,6 +29,7 @@ import jvx.numeric.PnMatrix;
 import jvx.project.PjWorkshop;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.SingularValueDecomposition;
 /**
@@ -87,14 +88,17 @@ public class Registration extends PjWorkshop {
 		}
 		
 		// Calculate min. distance between this set and points of Q.
-		HashMap<PdVector, PdVector> corresponding = new HashMap<PdVector, PdVector>();
+		
 		if(pointtoplane){
-			corresponding = GetClosestPointToPlaneDistances(vectors, m_surfQ);
+			HashMap<PdVector, HashMap<PdVector, Integer>> corresponding = GetClosestPointToPlaneDistances(vectors, m_surfQ);
+			pointToPlane(corresponding, m_surfQ);
 		}
 		else {
-			corresponding = GetClosestPointToPointDistances(vectors, m_surfQ);
+			// Calculate min. distance between this set and points of Q.
+			HashMap<PdVector, PdVector> corresponding = GetClosestPointToPointDistances(vectors, m_surfQ);
+			pointToPoint(corresponding);
 		}
-		
+		/*
 		// Compute centroid of P and Q.
 		PdVector pCentroid = computeCentroid(corresponding.keySet());
 		PdVector qCentroid = computeCentroid(corresponding.values());
@@ -122,6 +126,71 @@ public class Registration extends PjWorkshop {
 		PdMatrix rOpt = new PdMatrix(rOptInter.getData());
 		PdVector tOpt = PdVector.subNew(qCentroid, matrixMult(rOpt, pCentroid));
 		
+		rotateAndTranslate(m_surfP, rOpt, tOpt);*/
+	}
+	
+	public void pointToPlane(HashMap<PdVector, HashMap<PdVector, Integer>> corresponding, PgElementSet surface) {
+		// Get all vertex normals.
+		surface.makeVertexNormals();
+		PdVector[] normalsOfQ = surface.getVertexNormals();
+		
+		// Construct A.
+		RealMatrix A = constructA(corresponding, normalsOfQ);
+		RealMatrix AT = A.transpose();
+		
+		// Calculate (A^T*A)^-1.
+		RealMatrix ATA = AT.multiply(A);
+		RealMatrix ATAinv = new LUDecomposition(ATA).getSolver().getInverse();
+		
+		// Construct b.
+		RealMatrix b = constructB(corresponding, normalsOfQ);
+		
+		// Calculate x.
+		RealMatrix x = ATAinv.multiply(AT).multiply(b);
+		
+		// Calculate r and t.
+		PdVector r = new PdVector();
+		r.set(0, x.getEntry(0, 0));
+		r.set(1, x.getEntry(0, 1));
+		r.set(2, x.getEntry(0, 2));
+		
+		PdVector t = new PdVector();
+		t.set(0, x.getEntry(0, 3));
+		t.set(1, x.getEntry(0, 4));
+		t.set(2, x.getEntry(0, 5));
+		
+		// Update the mesh.
+		rotateAndTranslatePointToPlane(m_surfP, r, t);
+	}
+	
+	public void pointToPoint(HashMap<PdVector, PdVector> corresponding) {
+		// Compute centroid of P and Q.
+		PdVector pCentroid = computeCentroid(corresponding.keySet());
+		PdVector qCentroid = computeCentroid(corresponding.values());
+				
+		// Compute optimal rotation and translation.
+		PdMatrix m = new PdMatrix(3);
+		for(PdVector vector : corresponding.keySet()){
+			PdMatrix a = new PdMatrix(3);
+			a.adjoint(PdVector.subNew(vector, pCentroid), PdVector.subNew(corresponding.get(vector), qCentroid));
+			m.add(a);
+		}
+		double n2 = corresponding.keySet().size();
+		PdMatrix rotation = divide(m, n2);
+		
+		// Compute SVD.
+		Array2DRowRealMatrix realmatrix = new Array2DRowRealMatrix(rotation.m_data);
+		SingularValueDecomposition svd = new SingularValueDecomposition(realmatrix);
+				
+		double[][] inter = ones(3);
+				
+		RealMatrix uvt = svd.getU().multiply(svd.getVT());
+		PdMatrix vut = new PdMatrix(svd.getV().multiply(svd.getUT()).getData());
+		inter[2][2] = PnMatrix.determinant(vut.m_data, 3);
+		RealMatrix rOptInter = svd.getV().multiply(new Array2DRowRealMatrix(inter).multiply(svd.getUT()));
+		PdMatrix rOpt = new PdMatrix(rOptInter.getData());
+		PdVector tOpt = PdVector.subNew(qCentroid, matrixMult(rOpt, pCentroid));
+				
 		rotateAndTranslate(m_surfP, rOpt, tOpt);
 	}
 	
@@ -162,7 +231,6 @@ public class Registration extends PjWorkshop {
 		
 		// Remove all vectors with distance greater than k*meanDist.
 		double k = 2.0;
-		HashSet<PdVector> entrySet = new HashSet<PdVector>();
 		for(PdVector v : results.keySet()) {
 			if(results.get(v) > k*meanDist){
 				corresponding.remove(v);
@@ -172,32 +240,36 @@ public class Registration extends PjWorkshop {
 		return corresponding;
 	}
 
-	private HashMap<PdVector, PdVector> GetClosestPointToPlaneDistances(PdVector[] vectors, PgElementSet surface)
+	private HashMap<PdVector, HashMap<PdVector, Integer>> GetClosestPointToPlaneDistances(PdVector[] vectors, PgElementSet surface)
 	{
 		// Calculate min. distance between this set and points of Q.
 		HashMap<PdVector, Double> results = new HashMap<PdVector, Double>();
-		HashMap<PdVector, PdVector> corresponding = new HashMap<PdVector, PdVector>();
+		HashMap<PdVector, HashMap<PdVector, Integer>> corresponding = new HashMap<PdVector, HashMap<PdVector, Integer>>();
 		double meanDist = 0.0;
 
 		PdVector[] vectorsOfQ = surface.getVertices();
-		surface.makeVertexNormals();
-		PdVector[] normalsOfQ = surface.getVertexNormals();
+		//surface.makeVertexNormals();
+		//PdVector[] normalsOfQ = surface.getVertexNormals();
 
 		for(int j = 0; j < vectors.length; j++){
 			double minDist = Double.MAX_VALUE;
 			PdVector closestPoint = null;
+			int indexOfQ = -1;
 			for(int j2 = 0; j2 < vectorsOfQ.length; j2++){
-				PdVector point = PdVector.subNew(vectors[j], vectorsOfQ[j2]);
-				PdVector projectionOntoNormal = GetProjectionPOntoQ(point, normalsOfQ[j2]);
-				double dist = projectionOntoNormal.dot(projectionOntoNormal);
+				//PdVector point = PdVector.subNew(vectors[j], vectorsOfQ[j2]);
+				//PdVector projectionOntoNormal = GetProjectionPOntoQ(point, normalsOfQ[j2]);
+				double dist = PdVector.dist(vectors[j], vectorsOfQ[j2]);
 				if(dist < minDist){
 					minDist = dist;
 					closestPoint = vectorsOfQ[j2];
+					indexOfQ = j2;
 				}
 			}
 			meanDist += minDist;
 			results.put(vectors[j], minDist);
-			corresponding.put(vectors[j], closestPoint);
+			HashMap<PdVector, Integer> map = new HashMap<PdVector, Integer>();
+			map.put(closestPoint, indexOfQ);
+			corresponding.put(vectors[j], map);
 		}
 		meanDist = meanDist/vectors.length;
 
@@ -244,12 +316,12 @@ public class Registration extends PjWorkshop {
 		return new PdVector(results);
 	}
 	
-	private PdVector GetProjectionPOntoQ(PdVector p, PdVector q)
+	/*private PdVector GetProjectionPOntoQ(PdVector p, PdVector q)
 	{
 		PdVector result = new PdVector(q.m_data);
 		result.multScalar(p.dot(q) / q.dot(q));
 		return result;
-	}
+	}*/
 	
 	private void rotateAndTranslate(PgElementSet set, PdMatrix r, PdVector t)
 	{
@@ -261,5 +333,63 @@ public class Registration extends PjWorkshop {
 			set.setVertex(i, PdVector.addNew(matrixMult(r, old), t));
 		}
 		m_surfP.update(m_surfP);
+	}
+	
+	private void rotateAndTranslatePointToPlane(PgElementSet set, PdVector r, PdVector t)
+	{
+		System.out.println("Rotating and translating");
+		System.out.println(r);
+		System.out.println(t);
+		for(int i = 0; i < set.getNumVertices(); i++){
+			PdVector old = set.getVertex(i);
+			PdVector trans = PdVector.addNew(old, PdVector.crossNew(old, r));
+			trans = PdVector.addNew(trans, t);
+			set.setVertex(i, trans);
+		}
+		m_surfP.update(m_surfP);
+	}
+	
+	private RealMatrix constructA(HashMap<PdVector, HashMap<PdVector, Integer>> corresponding, PdVector[] normals){
+		// Constructing A as shown here: http://resources.mpi-inf.mpg.de/deformableShapeMatching/EG2011_Tutorial/slides/2.1%20Rigid%20ICP.pdfS
+		int n = corresponding.keySet().size();
+		RealMatrix res = new Array2DRowRealMatrix(n, 6);
+		int i = 0;
+		for(PdVector pi : corresponding.keySet()) {
+			HashMap<PdVector, Integer> results = corresponding.get(pi);
+			PdVector qi = results.keySet().iterator().next();
+			int indexOfNorm = results.get(qi);
+			PdVector crossRes = PdVector.crossNew(pi, normals[indexOfNorm]);
+			res.setEntry(i, 0, crossRes.getEntry(0));
+			res.setEntry(i, 1, crossRes.getEntry(1));
+			res.setEntry(i, 2, crossRes.getEntry(2));
+			res.setEntry(i, 3, pi.getEntry(0));
+			res.setEntry(i, 4, pi.getEntry(1));
+			res.setEntry(i, 5, pi.getEntry(2));
+			i++;
+		}
+		
+		return res;
+	}
+	
+	private RealMatrix constructB(HashMap<PdVector, HashMap<PdVector, Integer>> corresponding, PdVector[] normals){
+		// Constructing b as shown here: http://resources.mpi-inf.mpg.de/deformableShapeMatching/EG2011_Tutorial/slides/2.1%20Rigid%20ICP.pdfS
+		int n = corresponding.keySet().size();
+		RealMatrix res = new Array2DRowRealMatrix(n, 1);
+		int i = 0;
+		for(PdVector pi : corresponding.keySet()) {
+			HashMap<PdVector, Integer> results = corresponding.get(pi);
+			PdVector qi = results.keySet().iterator().next();
+			int indexOfNorm = results.get(qi);
+			PdVector norm = normals[indexOfNorm];
+			
+			PdVector diff = PdVector.subNew(pi, qi);
+			diff.multScalar(-1.0);
+			
+			double entry = diff.dot(norm);
+			res.setEntry(i, 0, entry);
+			i++;
+		}
+		
+		return res;
 	}
 }
